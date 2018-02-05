4d87f065e17c945824a9a3196206087ac521af2f
Darshan Bangoria <darshanbangoria.2@gmail.com>
2018-02-05 19:57:04 +0530

    Added model interaction

--- Angular/EMA/src/app/employee/employee.component.ts
+++ Angular/EMA/src/app/employee/employee.component.ts
@@ -13,18 +13,6 @@ export class EmployeeComponent implements OnInit {
   experiences: string[];
   languages: string[];
   empData;
-
-  // emp form
-  firstName: string;
-  lastName: string;
-  userEmail: string;
-  contactNo: string;
-  userAddress: string;
-  userName: string;
-  userPass: string;
-  gender: string;
-  qualify: string;
-  exp: string;
   lang: string[];
   constructor() { }
 
@@ -33,12 +21,11 @@ export class EmployeeComponent implements OnInit {
     this.experiences = ['1 Year', '2 Year', 'More than 2'];
     this.languages = ['C/C++', 'Java', 'C#', 'PHP', 'Python'];
     this.lang = [];
+    this.empData = new Employee();
   }
 
   getEmp() {
-    this.empData = new Employee(this.firstName, this.lastName, this.userEmail,
-                                this.contactNo, this.userAddress, this.userName,
-                                this.userPass, this.gender, this.qualify, this.exp, this.lang.toString());
+    this.empData.lang = this.lang.toString();
     this.printData(JSON.parse(JSON.stringify(this.empData)));
     this.lang.splice(0, this.lang.length);
     window.location.reload(true);
